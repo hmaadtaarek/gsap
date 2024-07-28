9093308a095306fac1b33f2018d82ae0a5b01d04
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-07-28 07:05:29 +0000

    repeact & yoyo & stagger

--- part 1/script.js
+++ part 1/script.js
@@ -1,15 +1,29 @@
+// var tl = gsap.timeline();
 
-var tl = gsap.timeline()
+// // Move box1 to the right
+// tl.to(".box1", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+// });
 
-tl.to(".box1",{
+// // Move box1 back to the original position using tl.from
+// tl.from(".box2", {
+//     x:1000,
+//     duration: 2,
+//     delay: 1,
+// });
+
+
+
+gsap.to(".box1, .box2",{
     x: 1000,
-    duration: 2,
-    delay: 1,
+    duration: 1,
+    delay:1,
+    rotate: 360,
+    stagger: 1,
+    repeat: -1,
+    yoyo: true
+    
+
 })
-tl.to(".box2",{
-    x: 1000,
-    y: 200,
-    duration: 2
-}
-
-)
